566d40e33bcd2cf2053d8a82fd25a954ffb0d591
Uday PS <uday.pratap@tradyon.ai>
2025-06-15 15:57:45 +0530

    puppeteer path

--- server/routes/get/pdf.js
+++ server/routes/get/pdf.js
@@ -562,6 +562,7 @@ router.get('/dbf-invoice/:series/:billNo', async (req, res) => {
 
     try {
       browser = await puppeteer.launch({
+        executablePath: process.platform === 'win32' ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' : process.platform === 'linux' ? '/usr/bin/brave-browser' : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
